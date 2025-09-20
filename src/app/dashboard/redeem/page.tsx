2839696c06248deffcae0faf5a6666faacc2cb49
Julian Montes Ramirez <damon.198623@gmail.com>
2025-09-20 02:40:08 +0000

    recuerda que se debe confirmar la compra de los productos adquiridos en

--- src/app/dashboard/redeem/page.tsx
+++ src/app/dashboard/redeem/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { PageHeader } from "@/components/dashboard/page-header";
 import { Button } from "@/components/ui/button";
